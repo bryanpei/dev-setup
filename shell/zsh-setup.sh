435a740442a0ab345327d8b8199a3bebb2e81c5e
bryanpei <xbryanpei@gmail.com>
2021-11-21 22:03:27 -0800

    Added shell setup

---
+++ shell/zsh-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if ! command -v zsh &> /dev/null
+then
+  echo "zsh is not installed."
+  exit
+fi
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/rupa/z.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/z
+
+cp .p10k.zsh $HOME/.p10k.zsh
+cp .zshrc $HOME/.zshrc
+
+source $HOME/.zshrc
